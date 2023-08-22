e6263adc072c3939914cef6c8b5a1f23e99c5512
Kenzeroid <kenzeroid@gmail.com>
2023-08-22 13:43:15 +0700

    Initial Commit

--- src/main/java/com/pemerintah/surat/service/SuratPerjalananService.java
+++ src/main/java/com/pemerintah/surat/service/SuratPerjalananService.java
@@ -13,11 +13,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class SuratPerjalananService implements SuratPerjalanan{
@@ -25,51 +26,40 @@ public class SuratPerjalananService implements SuratPerjalanan{
 
         suratPerjalananData(suratPerjalanan);
 
-        try (InputStream jasper = getClass().getResourceAsStream("/SuratPerjalananDinas.jrxml");
-             InputStream logo = getClass().getResourceAsStream("/img.png")) {
-            assert logo != null;
-            try (BufferedReader logoReader = new BufferedReader(new InputStreamReader(logo))) {
-                assert jasper != null;
-                try (BufferedReader jasperReader = new BufferedReader(new InputStreamReader(jasper))) {
-                    String reportDirectory = jasperReader.lines()
-                            .collect(Collectors.joining(System.lineSeparator()));
-                    String logoDirectory = jasperReader.lines()
-                            .collect(Collectors.joining(System.lineSeparator()));
-                    JasperReport jasperReport = JasperCompileManager.compileReport(reportDirectory);
-                    Map<String, Object> parameters = new HashMap<>();
-                    parameters.put("Nomor", suratPerjalanan.getNomor());
-                    parameters.put("Wenang", suratPerjalanan.getWenang());
-                    parameters.put("Nama", suratPerjalanan.getNama());
-                    parameters.put("Golongan", suratPerjalanan.getGolongan());
-                    parameters.put("Jabatan", suratPerjalanan.getJabatan());
-                    parameters.put("Tingkat", suratPerjalanan.getTingkat());
-                    parameters.put("Tujuan", suratPerjalanan.getTujuan());
-                    parameters.put("Kendaraan", suratPerjalanan.getKendaraan());
-                    parameters.put("TempatBerangkat", suratPerjalanan.getTempatBerangkat());
-                    parameters.put("TempatTujuan", suratPerjalanan.getTempatTujuan());
-                    parameters.put("LamaPerjalanan", suratPerjalanan.getLamaPerjalanan());
-                    parameters.put("TanggalBerangkat", suratPerjalanan.getTanggalBerangkat());
-                    parameters.put("TanggalKembali", suratPerjalanan.getTanggalKembali());
-                    parameters.put("Instansi", suratPerjalanan.getInstansi());
-                    parameters.put("MataAnggaran", suratPerjalanan.getMataAnggaran());
-                    parameters.put("Ditetapkan", suratPerjalanan.getDitetapkan());
-                    parameters.put("TanggalSurat", suratPerjalanan.getTanggalSurat());
-                    parameters.put("Maksud", suratPerjalanan.getMaksud());
-                    parameters.put("Logo", logoDirectory);
-                    //Fill Jasper report
-                    JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
-                    //Export report
-        //        JRDocxExporter exporter = new JRDocxExporter();
-        //        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-        //        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(httpServletResponse.getOutputStream()));
-        //        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=jasperfile.docx");
-        //        httpServletResponse.setContentType("application/octet-stream");
-        //        exporter.exportReport();
-                    JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
-                    return new ResponseEntity<>(HttpStatus.OK);
-                }
-            }
-        }
+        File file = ResourceUtils.getFile("classpath:SuratPerjalananDinas.jrxml");
+        File logo = ResourceUtils.getFile("classpath:img.png");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getPath());
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("Nomor", suratPerjalanan.getNomor());
+        parameters.put("Wenang", suratPerjalanan.getWenang());
+        parameters.put("Nama", suratPerjalanan.getNama());
+        parameters.put("Golongan", suratPerjalanan.getGolongan());
+        parameters.put("Jabatan", suratPerjalanan.getJabatan());
+        parameters.put("Tingkat", suratPerjalanan.getTingkat());
+        parameters.put("Tujuan", suratPerjalanan.getTujuan());
+        parameters.put("Kendaraan", suratPerjalanan.getKendaraan());
+        parameters.put("TempatBerangkat", suratPerjalanan.getTempatBerangkat());
+        parameters.put("TempatTujuan", suratPerjalanan.getTempatTujuan());
+        parameters.put("LamaPerjalanan", suratPerjalanan.getLamaPerjalanan());
+        parameters.put("TanggalBerangkat", suratPerjalanan.getTanggalBerangkat());
+        parameters.put("TanggalKembali", suratPerjalanan.getTanggalKembali());
+        parameters.put("Instansi", suratPerjalanan.getInstansi());
+        parameters.put("MataAnggaran", suratPerjalanan.getMataAnggaran());
+        parameters.put("Ditetapkan", suratPerjalanan.getDitetapkan());
+        parameters.put("TanggalSurat", suratPerjalanan.getTanggalSurat());
+        parameters.put("Maksud", suratPerjalanan.getMaksud());
+        parameters.put("Logo", logo.getPath());
+        //Fill Jasper report
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
+        //Export report
+//        JRDocxExporter exporter = new JRDocxExporter();
+//        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+//        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(httpServletResponse.getOutputStream()));
+//        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=jasperfile.docx");
+//        httpServletResponse.setContentType("application/octet-stream");
+//        exporter.exportReport();
+        JasperExportManager.exportReportToPdfStream(jasperPrint,httpServletResponse.getOutputStream());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public ResponseEntity<?> exportJasperReport(HttpServletResponse response, ExportJasper exportJasper) throws JRException, IOException {
